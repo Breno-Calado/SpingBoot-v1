e2da1952972a851c575fbf199b54159bc8499c8e
Breno <breno.calado@gmail.com>
2021-05-08 23:11:54 -0300

    deletando uma categoria com DELETE

--- src/main/java/com/brenomaia/cusromc/services/CategoriaService.java
+++ src/main/java/com/brenomaia/cusromc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.brenomaia.cusromc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.brenomaia.cusromc.domain.Categoria;
 import com.brenomaia.cusromc.repositories.CategoriaRepository;
+import com.brenomaia.cusromc.services.exceptions.DataIntegrityException;
 import com.brenomaia.cusromc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria categoriaObj) {
 		findByID(categoriaObj.getId());
 		return categoriaDAO.save(categoriaObj);
+	}
+	
+	public void delete(Integer id) {
+		
+		try {
+			categoriaDAO.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+			
+		}
 	}
 
 }
