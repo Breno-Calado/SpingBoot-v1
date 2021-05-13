e65cac7ef483f1d0c8870d9446a60d2b1ef62653
Breno <breno.calado@gmail.com>
2021-05-12 21:02:50 -0300

    Personalizado o retorno da validacao

---
+++ src/main/java/com/brenomaia/cusromc/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package com.brenomaia.cusromc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> erros =  new ArrayList<FieldMessage>();
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErros() {
+		return erros;
+	}
+
+	public void addError(String fieldName, String messagem) {
+		this.erros.add(new FieldMessage(fieldName, messagem));
+	}
+
+	
+}
